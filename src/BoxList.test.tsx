b6ea6c7faf7937cf16d66c48055d2542ac088540
Gerard Keiser <gerardkeiser@gmail.com>
2022-09-15 23:00:56 -0500

    Add remove box text

--- src/BoxList.test.tsx
+++ src/BoxList.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render, fireEvent, screen } from "@testing-library/react";
 import BoxList from "./BoxList";
 
 jest.mock("uuid", () => ({ v4: () => "123456789" }));
@@ -11,4 +11,12 @@ it("renders", () => {
 it("matches snapshot", () => {
   const { asFragment } = render(<BoxList />);
   expect(asFragment()).toMatchSnapshot();
+});
+
+it("can remove a box", () => {
+  render(<BoxList />);
+  const deleteButton = screen.getByText("X");
+  expect(deleteButton).toBeInTheDocument();
+  fireEvent.click(deleteButton);
+  expect(deleteButton).not.toBeInTheDocument();
 });
